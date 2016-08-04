316b970f148555e3cf4dd6a4d52621a346b4e5d7
ryzhykE <stoyatboy@gmail.com>
2016-08-04 22:52:36 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,6 @@
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
 </head>
 <body>
-	
     <script src="/js/app.js"></script>
     <script src='/js/controller.js'></script>
     <script src="/js/router.js"></script>
